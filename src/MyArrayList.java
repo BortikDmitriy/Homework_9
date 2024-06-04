ad6e4cdb4f352bcdfed7dd71db538f56b70ab072
BortikDmitriy <134203326+BortikDmitriy@users.noreply.github.com>
2024-06-04 22:01:41 +0300

    Initial commit

---
+++ src/MyArrayList.java
@@ -0,0 +1,72 @@
+public class MyArrayList {
+    private Object[] array;
+    private int size;
+
+    public MyArrayList() {
+        array = new Object[10];
+        size = 0;
+    }
+
+    public void add(Object value) {
+        ensureCapacity(size + 1);
+        array[size] = value;
+        size++;
+    }
+
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[size - 1] = null;
+        size--;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Object get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return array[index];
+    }
+
+    private void ensureCapacity(int minCapacity) {
+        if (minCapacity > array.length) {
+            int newCapacity = array.length * 2;
+            if (newCapacity < minCapacity) {
+                newCapacity = minCapacity;
+            }
+            Object[] newArray = new Object[newCapacity];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyArrayList list = new MyArrayList();
+        list.add("First");
+        list.add("Second");
+        list.add("Third");
+        System.out.println("Size: " + list.size());
+        System.out.println("Get index 1: " + list.get(1));
+        list.remove(1);
+        System.out.println("Size after removal: " + list.size());
+        System.out.println("Get index 1 after removal: " + list.get(1));
+        list.clear();
+        System.out.println("Size after clear: " + list.size());
+    }
+}
